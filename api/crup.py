6b552a61e7aa6ea03ce2c81b43c134447d257516
ederson457 <stive98765@gmail.com>
2024-09-23 21:51:03 -0500

    Create crup.py

---
+++ api/crup.py
@@ -0,0 +1,11 @@
+'''
+c=> Create (insert into)
+R=> Read (select)
+U=> update (update)
+D=> Delete (delete)
+
+update and delete need a where clause *
+'''
+
+def create_user():
+    new_user_query = "INSERT INTO users ()"
