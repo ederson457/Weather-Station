72855aa0bff78d2e54a75c4f019a2c810a5d6618
Jhonny942 <stive98765@gmail.com>
2024-10-24 21:50:54 -0500

    v3

---
+++ api/detect_arduino_port.py
@@ -0,0 +1,27 @@
+
+
+import serial
+
+def get_arduino_port():
+    ports = serial.tools.list_ports.comports()
+    print (ports)
+    for port in ports:
+        if "Arduino" in port.description or "USB-SERIAL CH340":
+          print(f"Detected port: {port.device}")
+          print(f"Name: {port.name}")
+          print(f"Description: {port.description}")
+          print(f"HWID: {port.hwid}")
+          print(f"PID: {port.pid}")
+          print(f"Serial Number: {port.serial}")
+          print(f"Manufacturer: {port.manufacturer}")
+          print(f"Product: {port.product}")
+          print(f"Interfaces: {port.interfaces}")
+          print(f"Locactions: {port.locactions}")
+
+    return ports.device
+print("No arduino port detected")
+return None
+
+p=get_arduino_port()
+print (p)
+
--- api/get_arduino_data.py
+++ api/get_arduino_data.py
@@ -7,12 +7,14 @@ Developer: Ederson Valencia
 '''
 
 #Import libraries 
-
-
-
 #Arduino port
-arduino_port = "COM11"
-arduino_bau=9600
+import serial
+import time
+import serial.tools.list_ports # type: ignore
+from detect_arduino_port import p
+
+arduino_port = p
+arduino_bau = 9600
 
 service = serial.Serial(
     arduino_port,
@@ -28,8 +30,14 @@ while True:
     
     if data:
         print(data)
-        #temperature, humidity =data.split(",")
+        temperature, humidity =data.split(",")
+        print(f"Temperature:{temperature}°C")
+        print(f"humidity:{humidity}%")    
         
-        #print(f"Temperature:{temperature}°C")
-        #print(f"humidity:{humidity}%")    
+        
+        #1. create a new model data called test_data
+        #fields; id, temp, hum, created_at
+        # 2. Create a method to insert data when detect changes in temp or hum
+        # 3. Update method: insert data detect changes in temp or hum 
+        
 time.sleep(1)
